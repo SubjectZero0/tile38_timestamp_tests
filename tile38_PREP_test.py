15bf9b8ec218c21fcd592065982d5c031ca80504
Alexandrer Iordanou <iordanoualexander@gmail.com>
2022-12-22 03:34:13 +0200

    Restructures preparatory test

---
+++ tile38_PREP_test.py
@@ -0,0 +1,54 @@
+import geojson
+import redis
+import ast
+
+
+#-------------------------------------------------------
+
+def to_dict(a):
+    #helper function to convert bytes object to dict
+    return ast.literal_eval(a.decode('utf-8'))
+
+#--------------------PREP TESTS-------------------------
+
+def test_create_point():
+    """
+    General tile38 Test
+    """
+    #set the client to redis. port 9851 for tile38
+    client = redis.Redis(host='127.0.0.1', port=9851)
+
+    client.execute_command("DROP", "fleet")
+
+    # insert data
+    result = client.execute_command('SET', 'fleet', 'truck', 'POINT', 33.32, 115.423)
+   
+    #Assert the SET command is executed
+    assert result == True
+    
+    #get the result point as a dictionary
+    point = to_dict(client.execute_command('GET', 'fleet', 'truck'))
+    
+    #assert the points coordinates are as expected
+    assert [115.423,33.32] in point.values()
+
+
+
+#------------------END OF PREP TESTS-------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    test_create_point()
+    
